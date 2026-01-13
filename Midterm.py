e27847e064b15c105a97102f582f0eed86678217
RubyBlumel <rkblumel@gmail.com>
2026-01-13 11:19:54 -0500

    Update Midterm.py

--- Midterm.py
+++ Midterm.py
@@ -9,20 +9,26 @@ def area_of_circle(radius):
 def hollow_right_triangle(n):
     if n < 4:
         return "The triangle height should be at least 4."
-    else:
+    elif n == 4:
         #top is the same for all 
+        x = "*"
         top1 = "*"
         top2 = "**"
+        top3 = "* *"
+       
+        return f"{top1}\n{top2}\n{top3}\n{x * n}"
+        
+    else:
+        top1 = "*"
+        top2 = "**"
+        top3 = "* *"
         # middle
         x = "*"
         y = " "
-        middle1 = f"{x}{y}{x}"
-        middle2 = ""
-        for i in range(3, (n + 1)):
-            middle2 = f"{x}{y * (n - i)}{x}"
-        #bottom
-        bottom = f"{x * n}"
-    return f"{top1}\n{top2}\n{middle1}\n{middle2}\n{bottom}"
+        middle2 = "*"
+        for i in range(3, n):
+            middle2 = "  " * (n - i - 1)
+            return f"{top1}\n{top2}\n{top3}\n{x}{middle2}{x}\n{x * n}"
 
 
 # Q3: Inverted Pyramid
@@ -30,4 +36,4 @@ def inverted_pyramid(n):
     return ""
 
 # ----------------------------------------------------------------
-print(hollow_right_triangle(6))
+print(hollow_right_triangle(4))
